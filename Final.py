dbeacfd781509c42145a5dc5d0b9c73be964507d
ajgramli <153561080+ajgramli@users.noreply.github.com>
2025-04-30 17:41:29 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,3 +1,10 @@
+"""
+Spotify API Music Recommendation Project
+By: Arthur Gramlich
+CIS 351
+"""
+
+
 import requests
 import base64
 
@@ -18,7 +25,7 @@ def SpotifyToken():
 
     response = requests.post(TokenUrl, headers=headers, data=data) #requests access token from token URL
     AccessToken = response.json().get('access_token') #gets access token and converts it from json format 
-    return AccessToken 
+    return AccessToken  #Make sure to return the token
 
 def preferences():  #get user preferences for search parameters
     artist = input("What artist would you like recommendations for? ").capitalize()
